5e78ce9af0d6e596e4990469d1938c50d1fbe6b8
Tharindu <tharindui@labfriend.com>
2022-12-02 14:29:46 +0530

    pgrade-insecure-requests

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
