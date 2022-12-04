9868a0450f414f425c32f3f3a2bd3adce736d6b3
Tharindu <tharindui@labfriend.com>
2022-12-04 13:48:46 +0530

    insecure-requests

--- src/shared/services/employee/index.ts
+++ src/shared/services/employee/index.ts
@@ -3,27 +3,26 @@ import axios from "axios";
 
 const employeeApi = axios.create({
   //baseURL: "http://localhost:9999",
-  baseURL: "",
+  baseURL: "http://52.194.235.87:9999",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Content-Security-Policy": "upgrade-insecure-requests",
   },
 });
 
 export const getAllEmployeesAPI = async () => {
-  const response = await employeeApi.get("http://52.194.235.87:9999/employee");
+  const response = await employeeApi.get("/employee");
   console.log("getAllEmployees");
   return response.data;
 };
 
 export const postEmployeeAPI = async (employee: employeeDetailsType) => {
-  return await employeeApi.post("http://52.194.235.87:9999/employee", employee);
+  return await employeeApi.post("/employee", employee);
 };
 
 export const getEmployeeByIdAPI = async (empId: string) => {
-  const response = await employeeApi.get(
-    `http://52.194.235.87:9999/employee/${empId}`
-  );
+  const response = await employeeApi.get(`/employee/${empId}`);
   return response.data;
 };
 
@@ -34,10 +33,7 @@ export const deleteEmployeeByIdAPI = async (empId: number) => {
 };
 
 export const updateEmployeeAPI = async (employee: employeeDetailsType) => {
-  return await employeeApi.patch(
-    `http://52.194.235.87:9999/employee/${employee._id}`,
-    employee
-  );
+  return await employeeApi.patch(`/employee/${employee._id}`, employee);
 };
 
 export default employeeApi;
