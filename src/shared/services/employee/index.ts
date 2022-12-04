97bd5c5f374c525a075553c8d4d28262c8ddbe04
Tharindu <tharindui@labfriend.com>
2022-12-04 11:43:09 +0530

    sss

--- src/shared/services/employee/index.ts
+++ src/shared/services/employee/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const employeeApi = axios.create({
   //baseURL: "http://localhost:9999",
-  baseURL: "http://52.194.235.87:9999",
+  baseURL: "",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
@@ -11,26 +11,33 @@ const employeeApi = axios.create({
 });
 
 export const getAllEmployeesAPI = async () => {
-  const response = await employeeApi.get("/employee");
+  const response = await employeeApi.get("http://52.194.235.87:9999/employee");
   console.log("getAllEmployees");
   return response.data;
 };
 
 export const postEmployeeAPI = async (employee: employeeDetailsType) => {
-  return await employeeApi.post("/employee", employee);
+  return await employeeApi.post("http://52.194.235.87:9999/employee", employee);
 };
 
 export const getEmployeeByIdAPI = async (empId: string) => {
-  const response = await employeeApi.get(`/employee/${empId}`);
+  const response = await employeeApi.get(
+    `http://52.194.235.87:9999/employee/${empId}`
+  );
   return response.data;
 };
 
 export const deleteEmployeeByIdAPI = async (empId: number) => {
-  return await employeeApi.delete(`/employee/${empId}`);
+  return await employeeApi.delete(
+    `http://52.194.235.87:9999/employee/${empId}`
+  );
 };
 
 export const updateEmployeeAPI = async (employee: employeeDetailsType) => {
-  return await employeeApi.patch(`/employee/${employee._id}`, employee);
+  return await employeeApi.patch(
+    `http://52.194.235.87:9999/employee/${employee._id}`,
+    employee
+  );
 };
 
 export default employeeApi;
