82efef3cbd25f4b7b862291fe30c0225b704c561
Tharindu <tharindui@labfriend.com>
2022-12-06 10:32:28 +0530

    validation

--- src/shared/validationSchema/employeeSchema.ts
+++ src/shared/validationSchema/employeeSchema.ts
@@ -3,8 +3,8 @@ const phoneNumberRegex =
   /^(?:0|94|\+94)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|912)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$/;
 export const employeeSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
+  firstName: Yup.string().min(6,'min 6 character').max(10,'max 10 characters').required("Required"),
+  lastName: Yup.string().required("Required").min(6,'min 6 character').max(10,'max 10 characters'),
   number: Yup.string()
     .matches(phoneNumberRegex, "Phone number is not valid")
     .required("Required"),
