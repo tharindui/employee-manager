ebbe5d802cf51b5fa55034daadfc9f5ba76c046d
Tharindu <tharindui@labfriend.com>
2022-12-06 10:48:30 +0530

    only letter and space

--- src/shared/validationSchema/employeeSchema.ts
+++ src/shared/validationSchema/employeeSchema.ts
@@ -1,10 +1,12 @@
 import * as Yup from "yup";
 const phoneNumberRegex =
   /^(?:0|94|\+94)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|912)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\d)\d{6}$/;
+
+  var stringRegex = new RegExp("^[a-zA-Z ]+$");
 export const employeeSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
-  firstName: Yup.string().required("Required").min(6,'min 6 characters').max(10,'max 10 characters'),
-  lastName: Yup.string().required("Required").min(6,'min 6 characters').max(10,'max 10 characters'),
+  firstName: Yup.string().required("Required").min(6,'min 6 characters').max(10,'max 10 characters').matches(stringRegex, "Phone number is not valid"),
+  lastName: Yup.string().required("Required").min(6,'min 6 characters').max(10,'max 10 characters').matches(stringRegex, "Phone number is not valid"),
   number: Yup.string()
     .matches(phoneNumberRegex, "Phone number is not valid")
     .required("Required"),
